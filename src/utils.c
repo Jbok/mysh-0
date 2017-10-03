45576916fde3a2677178b168a0715a328764e9eb
bok <0614bok@gmail.com>
2017-10-03 12:10:47 +0900

    set Function 1: mysh_parse_command();

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,25 @@
+#include <stdio.h>
+#include <string.h>
 #include "utils.h"
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+	int length = strlen(command);
+	
+	char buffer[8096]; //char array temporary save word
+	strcpy(buffer,command); //save string
+
+	int wordcount=0;
+	char *wordArray[8096]={};//string array temporary save words
+
+	char *ptr=strtok(buffer," ");//pointer of word divided by " "
+	
+	while(ptr!=NULL){
+		wordArray[wordcount++]=ptr;
+		ptr=strtok(NULL," ");
+	}
+
+	*argv=wordArray;
+	*argc=wordcount;
 }
