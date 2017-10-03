00876440581e2546ae62c29bbbf14426cb70d257
bok <0614bok@gmail.com>
2017-10-03 17:27:56 +0900

    add condition to validate function wheter argv is "cd" or "pwd"

--- src/commands.c
+++ src/commands.c
@@ -22,14 +22,14 @@ int do_pwd(int argc, char** argv) {
 }
 
 int validate_cd_argv(int argc, char** argv) {
-	if(argc==2)
+	if(argc==2 && !strcmp(*argv,"cd"))
 		return 1;
 	else
 		return 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-	if(argc==1)
+	if(argc==1 && !strcmp(*argv,"pwd"))
 		return 1;
 	else
 		return 0;
