0d5dc8b67b90ee4c51201a277fa68d48932f6217
bok <0614bok@gmail.com>
2017-10-04 13:32:33 +0900

    if there is no word in input sentence return argc=1 argv[0]="\0"

--- src/utils.c
+++ src/utils.c
@@ -16,7 +16,7 @@ void mysh_parse_command(const char* command,
 	}
 
 	int wordcount=0;
-	char *wordArray[8096]={};//string array temporary save words
+	char *wordArray[8096]={""};//string array temporary save words
 
 	char *ptr=strtok(buffer," ");//pointer of word divided by " "
 	
@@ -27,6 +27,11 @@ void mysh_parse_command(const char* command,
 		ptr=strtok(NULL," ");
 	}
 
+	//if there is no word add "\0" and wordcount=1
+	if(wordcount==0){
+		wordcount=1;
+	}
+	
 	//2D dynamic allocate
 	*argv=(char**)malloc(sizeof(char*)*wordcount);
 	for(int i=0;i<wordcount;i++){
